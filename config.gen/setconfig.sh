cbf68d493ec4b5151724956f0b26c3b3d8980565
ferry133 <ferry133@gmail.com>
2026-03-05 00:41:57 +0800

    1

--- config.gen/setconfig.sh
+++ config.gen/setconfig.sh
@@ -1,3 +1,7 @@
+cp ./config.gen/cluster.yaml .
+cp ./config.gen/nodes.yaml .
+cp ./config.gen/cloudflare-tunnel.json .
+
 mkdir $PWD/talos/clusterconfig
 cp ./config.gen/*kubeconfig.yaml $KUBECONFIG
 cp ./config.gen/*talosconfig.yaml $TALOSCONFIG
